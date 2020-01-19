e6f792f8fc3b10c706727722e462027a58d8731d
Esteban Garces <AnthonyGarces@users.noreply.github.com>
2020-01-19 10:40:31 -0600

    created burger model

---
+++ models/burger.js
@@ -0,0 +1,9 @@
+module.exports = function(sequelize, DataTypes) {
+
+    const Burger = sequelize.define('Burger', {
+        burger_name: DataTypes.STRING,
+        devoured: DataTypes.BOOLEAN, 
+    });
+
+    return Burger;
+}
