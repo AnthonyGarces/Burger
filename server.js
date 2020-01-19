239928f895a6fa81a92020749fd459c08fbe6b39
Esteban Garces <AnthonyGarces@users.noreply.github.com>
2020-01-19 12:41:13 -0600

    Set up express, set a static directory, and wrote code to start the server

--- server.js
+++ server.js
@@ -1,3 +1,33 @@
 //Starting point for the node/express servers
 
+//Dependencies
+//==============================================================
 var express = require("express");
+const db = require('./models');
+
+//Sets up express
+//==============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+//Sets up the Express App to handle data parsing
+//==============================================================
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Static directory to be served
+//==============================================================
+app.use(express.static("app/public"));
+
+//Routes
+//==============================================================
+
+require("./app/routes/html-routes.js")(app)
+
+//Starts the server to begin listening
+//==============================================================
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function(){
+        console.log("app listening on PORT " + PORT);
+    })
+})
